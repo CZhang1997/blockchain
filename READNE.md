1a4088064396277c0e9298d5fee0b2865a0a342e
Churong Zhang <cxz173430@utdallas.edu>
2021-09-06 17:35:56 -0500

    add is valid chain check

--- READNE.md
+++
@@ -1,7 +0,0 @@
-# Immutatble Ledger: change one block, then the rest of block that is following that block's link will be break
-
-# Distributed P2P Network: computer in the networks are connected, and save a copy of the blockchain
-
-# Byzentine Fault Tolerance
-
-# Consensus Protocol
--- blockchain.py
+++ blockchain.py
@@ -93,4 +93,10 @@ def get_chain():
                 'length': len(blockchain.chain)}
     return jsonify(response), 200
 
+@app.route('/is_valid_chain', methods =  ['GET'])
+def is_valid_chain():
+    response = {'is_valid': blockchain.is_chain_valid(blockchain.chain), 
+                }
+    return jsonify(response), 200
+
 app.run(host = '0.0.0.0', port= 5000)
